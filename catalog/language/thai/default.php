dd616c8436d2a805695500a19d184a1549c2c446
Chatchai Kaewdok <mr.kaewdok@gmail.com>
2014-12-11 17:26:03 +0700

    Lng: Thai language

---
+++ catalog/language/thai/default.php
@@ -0,0 +1,69 @@
+<?php
+// Locale
+$_['code']                  = 'th';
+$_['direction']             = 'ltr';
+$_['date_format_short']     = 'd/m/Y';
+$_['date_format_long']      = 'l dS F Y';
+$_['time_format']           = 'h:i:s A';
+$_['datetime_format']       = 'd/m/Y H:i:s';
+$_['decimal_point']         = '.';
+$_['thousand_point']        = ',';
+
+// Text
+$_['text_home']             = '<i class="fa fa-home"></i>';
+$_['text_yes']              = 'ตกลง';
+$_['text_no']               = 'ยกเลิก';
+$_['text_none']             = ' --- None --- ';
+$_['text_select']           = ' --- กรุณาเลือก --- ';
+$_['text_all_zones']        = 'ทั้งหมด';
+$_['text_pagination']       = 'แสดงผล %d ถึง %d หน้า %d (ทั้งหมด %d หน้า)';
+$_['text_loading']          = 'กำลังโหลดข้อมูล...';
+
+// Buttons
+$_['button_address_add']    = 'เพิ่มที่อยู่';
+$_['button_back']           = 'ย้อนกลับ';
+$_['button_continue']       = 'ดำเนินการต่อ';
+$_['button_cart']           = 'เพิ่มสินค้าในตะกร้า';
+$_['button_cancel']         = 'ยกเลิก';
+$_['button_compare']        = 'เปรียบเทียบสินค้า';
+$_['button_wishlist']       = 'เพิ่มรายการโปรด';
+$_['button_checkout']       = 'ชำระสินค้า';
+$_['button_confirm']        = 'ยืนยันการสั่งซื้อ';
+$_['button_coupon']         = 'ใช้คูปองส่วนลด';
+$_['button_delete']         = 'ลบรายการ';
+$_['button_download']       = 'ดาวน์โหลด';
+$_['button_edit']           = 'แก้ไข';
+$_['button_filter']         = 'Refine Search';
+$_['button_new_address']    = 'ที่อยู่ใหม่';
+$_['button_change_address'] = 'เปลี่ยนที่อยู่';
+$_['button_reviews']        = 'แสดงความคิดเห็น';
+$_['button_write']          = 'เขียนข้อความ';
+$_['button_login']          = 'เข้าสู่ระบบ';
+$_['button_update']         = 'แก้ไข';
+$_['button_remove']         = 'ลบรายการ';
+$_['button_reorder']        = 'Reorder';
+$_['button_return']         = 'คืน';
+$_['button_shopping']       = 'สั่งซื้อสินค้าเพิ่ม';
+$_['button_search']         = 'ค้นหา';
+$_['button_shipping']       = 'ยืนยัน';
+$_['button_submit']         = 'ยืนยัน';
+$_['button_guest']          = 'Guest Checkout';
+$_['button_view']           = 'ดูข้อมูล';
+$_['button_voucher']        = 'Apply Voucher';
+$_['button_upload']         = 'อัปโหลดไฟล์';
+$_['button_reward']         = 'Apply Points';
+$_['button_quote']          = 'Get Quotes';
+$_['button_list']           = 'แสดงผลแนวนอน';
+$_['button_grid']           = 'แสดงผลแบบตาราง';
+$_['button_map']            = 'เปิด Google Map';
+
+// Error
+$_['error_exception']       = 'Error Code(%s): %s in %s on line %s';
+$_['error_upload_1']        = 'Warning: The uploaded file exceeds the upload_max_filesize directive in php.ini!';
+$_['error_upload_2']        = 'Warning: The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form!';
+$_['error_upload_3']        = 'Warning: The uploaded file was only partially uploaded!';
+$_['error_upload_4']        = 'Warning: No file was uploaded!';
+$_['error_upload_6']        = 'Warning: Missing a temporary folder!';
+$_['error_upload_7']        = 'Warning: Failed to write file to disk!';
+$_['error_upload_8']        = 'Warning: File upload stopped by extension!';
+$_['error_upload_999']      = 'Warning: No error code available!';
