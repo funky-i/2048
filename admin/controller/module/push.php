190225d1a46192588cd032f8d8d414cf869b3d6e
Chatchai Kaewdok <mr.kaewdok@gmail.com>
2015-01-05 18:57:32 +0700

    Module: Push Notification

---
+++ admin/controller/module/push.php
@@ -0,0 +1,103 @@
+<?php
+class ControllerModulePush extends Controller {
+	private $error = array();
+
+	public function index() {
+		$this->load->language('module/push');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->load->model('setting/setting');
+
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			
+			$this->model_setting_setting->editSetting('push', $this->request->post);
+
+			$this->session->data['success'] = $this->language->get('text_success');
+
+			$this->response->redirect($this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'));
+		}
+
+		$data['heading_title'] = $this->language->get('heading_title');
+		
+		$data['text_edit'] = $this->language->get('text_edit');
+		$data['text_enabled'] = $this->language->get('text_enabled');
+		$data['text_disabled'] = $this->language->get('text_disabled');
+		
+		$data['entry_pem'] = $this->language->get('entry_pem');
+		$data['entry_password'] = $this->language->get('entry_password');
+		$data['entry_status'] = $this->language->get('entry_status');
+		$data['entry_mode'] = $this->language->get('entry_mode');
+
+		$data['help_pem'] = $this->language->get('help_pem');
+		$data['help_password'] = $this->language->get('help_password');
+
+		$data['button_save'] = $this->language->get('button_save');
+		$data['button_cancel'] = $this->language->get('button_cancel');
+
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], 'SSL')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_module'),
+			'href' => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL')
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('module/push', 'token=' . $this->session->data['token'], 'SSL')
+		);
+
+		$data['action'] = $this->url->link('module/push', 'token=' . $this->session->data['token'], 'SSL');
+
+		$data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
+
+		if (isset($this->request->post['push_mode'])) {
+			$data['push_mode'] = $this->request->post['push_mode'];
+		} else {
+			$data['push_mode'] = $this->config->get('push_mode');
+		}
+
+		if (isset($this->request->post['push_status'])) {
+			$data['push_status'] = $this->request->post['push_status'];
+		} else {
+			$data['push_status'] = $this->config->get('push_status');
+		}
+
+		if (isset($this->request->post['push_pem'])) {
+			$data['push_pem'] = $this->request->post['push_pem'];
+		} else {
+			$data['push_pem'] = $this->config->get('push_pem');
+		}
+
+		if (isset($this->request->post['push_password'])) {
+			$data['push_password'] = $this->request->post['push_password'];
+		} else {
+			$data['push_password'] = $this->config->get('push_password');
+		}		
+		
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+		$this->response->setOutput($this->load->view('module/push.tpl', $data));
+	}
+
+	protected function validate() {
+		if (!$this->user->hasPermission('modify', 'module/push')) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
+
+		return !$this->error;
+	}
+}
---
+++ admin/language/english/module/push.php
@@ -0,0 +1,20 @@
+<?php
+// Heading
+$_['heading_title']    = 'Push Notification';
+
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified push notification module!';
+$_['text_edit']        = 'Edit push notification Module';
+
+// Entry
+$_['entry_pem']     		= 'Push Pem';
+$_['entry_password']     	= 'Push Password';
+$_['entry_status']     	= 'Status';
+$_['entry_mode']     	= 'Test Mode';
+
+// Help
+$_['help_pem']			= 'Developer: goto <a href="https://developer.apple.com/library/mac/documentation/IDEs/Conceptual/AppDistributionGuide/MaintainingCertificates/MaintainingCertificates.html" target="_blank">developer apple</a>';
+$_['help_password']		= 'Help: goto <a href="http://faq.appaloosa-store.com/knowledgebase/articles/61785-how-to-generate-push-notification-p12-file">appaloosa</a>';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify push notification module!';
